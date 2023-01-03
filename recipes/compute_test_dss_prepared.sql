c4d2f242cb7c3402befcdec4d143a145911670a2
admin <admin>
2023-01-03 15:43:45 +0000

    Convert sql_query recipe to sql recipe: 42037.compute_test_dss_prepared

---
+++ recipes/compute_test_dss_prepared.sql
@@ -0,0 +1,8 @@
+SELECT 
+    "creation_date",
+    "office"
+  FROM "dbo"."test_dss" "__input_table"
+  WHERE CASE 
+      WHEN "creation_date" IS NULL THEN false
+      ELSE ("creation_date" >= '2022-12-01 00:00:00' AND "creation_date" IS NOT NULL) AND ("creation_date" < '2023-01-01 00:00:00' AND "creation_date" IS NOT NULL)
+    END
